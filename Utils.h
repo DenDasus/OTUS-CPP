cdfdbd6054ec2e24bbdb8abef86c65a1e06ff304
DenisK <kuzovin@gmail.com>
2019-08-06 15:57:46 +0300

    Add Utils class, move split method

---
+++ Utils.h
@@ -0,0 +1,41 @@
+//
+// Created by DenisK on 06.08.2019.
+//
+
+#ifndef OTUS_CPP_UTILS_H
+#define OTUS_CPP_UTILS_H
+
+#include <vector>
+
+class Utils {
+public:
+
+    // ("",  '.') -> [""]
+    // ("11", '.') -> ["11"]
+    // ("..", '.') -> ["", "", ""]
+    // ("11.", '.') -> ["11", ""]
+    // (".11", '.') -> ["", "11"]
+    // ("11.22", '.') -> ["11", "22"]
+    static std::vector<std::string> split(const std::string &str, char delim)
+    {
+        std::vector<std::string> parts;
+        auto start = 0;
+        auto stop = str.find_first_of(delim);
+
+        while(stop != std::string::npos)
+        {
+            parts.push_back(str.substr(start, stop - start));
+
+            start = stop + 1;
+            stop = str.find_first_of(delim, start);
+        }
+
+        parts.push_back(str.substr(start));
+
+        return parts;
+    }
+
+};
+
+
+#endif //OTUS_CPP_UTILS_H
--- main.cpp
+++ main.cpp
@@ -4,30 +4,9 @@
 #include <string>
 #include <vector>
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
-{
-    std::vector<std::string> r;
+#include "IpFilter.h"
+#include "Utils.h"
 
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
-
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
-    }
-
-    r.push_back(str.substr(start));
-
-    return r;
-}
 
 int main(int argc, char const *argv[])
 {
@@ -37,8 +16,8 @@ int main(int argc, char const *argv[])
 
         for(std::string line; std::getline(std::cin, line);)
         {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            std::vector<std::string> v = Utils::split(line, '\t');
+            ip_pool.push_back(Utils::split(v.at(0), '.'));
         }
 
         // TODO reverse lexicographically sort
