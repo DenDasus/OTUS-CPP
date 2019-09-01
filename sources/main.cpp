b086dac6d7f683d8f16a275d72b85c57a9424cb0
DenisK <kuzovin@gmail.com>
2019-09-01 18:30:31 +0300

    second cmake update

--- sources/main.cpp
+++ sources/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[])
 
     //Sorted
     for (IpAddr addr : ipPool) {
-        std::cout << addr << std::endl;
+        std::cout << addr << std::endl;  
     }
 
     // filter by first byte and output
